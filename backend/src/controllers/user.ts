e4b02ef04b4696bb05902c19ea96ff48038d1944
Aditya Kumar Saini <adityasainics10@gmail.com>
2024-06-23 20:02:01 +0530

    creaded user routes

--- backend/src/controllers/user.ts
+++ backend/src/controllers/user.ts
@@ -14,7 +14,20 @@ export const newUser =TryCatch(
             //return next( new ErrorHandler("mera custom erroe",400) )
              
             const {name,email,photo,gender,_id,dob}=req.body;
-            const user =await User.create({
+
+            if(!name || !email || !photo || !gender || !_id || !dob){
+                next(new ErrorHandler("Please add all fiels ",400))
+            }
+
+            let user = await User.findById(_id);
+            if(user){
+                return res.status(200).json({
+                    success:true,
+                    message:`Welcome , ${user.name}`
+                })
+            }
+
+             user =await User.create({
                 name,email,photo,gender,_id,
                 dob:new Date(dob)
             })
@@ -26,3 +39,53 @@ export const newUser =TryCatch(
     
     }
 );
+
+export const getAllUsers = TryCatch(
+    async (
+        req,res,next
+    )=>{
+        const users = await User.find({});
+        return res.status(200).json({
+            success:true,
+            users:users
+        })
+    }
+)
+
+export const getUser =TryCatch(
+    async(req,res,next)=>{
+       
+        const id = req.params.id;
+        const user = await User.findById(id);
+        
+
+        if(!user){
+            return next(new ErrorHandler("User not find with given id",400))
+        }
+
+        return res.status(200).json({
+            succes:true,
+            user:user
+        })
+    }
+)
+
+export const deleteUser = TryCatch(
+    async(req,res,next)=>{
+
+        const id = req.params.id;
+        const user = await User.findById(id);
+        if(!user){
+            return next(new ErrorHandler("User not find with given id",400))
+        }
+
+        await user.deleteOne();
+
+        return res.status(200).json({
+            succes:true,
+            message:"user deleted successfully"
+        })
+
+
+    }
+)
--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,12 +1,20 @@
 import express from "express";
 import {
 
+    deleteUser,
+    getAllUsers,
+  getUser,
   newUser,
 } from "../controllers/user.js";
 
 const app = express.Router();
 
 app.post("/new", newUser);
+app.get("/all",getAllUsers);
+app.get("/:id",getUser);
+app.delete("/:id",deleteUser);
+
+
 
 
 
