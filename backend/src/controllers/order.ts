9365687278b67b17a6d02a4ac5cd5dab2ecc37dd
Aditya Kumar Saini <adityasainics10@gmail.com>
2024-06-26 11:58:58 +0530

    created route to get single order detail

--- backend/src/controllers/order.ts
+++ backend/src/controllers/order.ts
@@ -4,6 +4,7 @@ import { NewOrderRequestBody } from "../types/types.js";
 import { Order } from "../models/order.js";
 import { invalidateCache, reduceStock } from "../utils/features.js";
 import { myCache } from "../app.js";
+import ErrorHandler from "../utils/utility-class.js";
 
 export const newOrder= TryCatch(
     async (req:Request<{},{},NewOrderRequestBody>,res,next)=>{
@@ -71,8 +72,31 @@ export const allOrders = TryCatch(
         if(myCache.has(`all-orders`)) 
             orders =JSON.parse(myCache.get(`all-orders`) as string) ;
         else{
-            orders = await Order.find()
+            orders = await Order.find().populate("user")
             myCache.set(`all-orders`,JSON.stringify(orders))
+        }
+
+        return res.status(200).json({
+            success:true,
+            orders
+        })
+    }
+)
+
+export const getSingleOrder = TryCatch(
+    async (req,res ,next)=>{
+        const {id} = req.params;
+        const key = `order-${id}`
+        let orders ;
+        if(myCache.has(key)) 
+            orders =JSON.parse(myCache.get(key) as string) ;
+        else{
+            orders = await Order.findById(id).populate("user","name")
+            if(!orders){
+                return next(new ErrorHandler("order not found",404))
+            }
+            myCache.set(key,JSON.stringify(orders))
+            
         }
 
         return res.status(200).json({
--- backend/src/routes/orders.ts
+++ backend/src/routes/orders.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import { adminOnly } from "../middlewares/auth.js";
-import { allOrders, myOrders, newOrder } from "../controllers/order.js";
+import { allOrders, getSingleOrder, myOrders, newOrder } from "../controllers/order.js";
 
 const app = express.Router();
 
 app.post("/new", newOrder);
 app.get('/my',myOrders)
 app.get('/all',adminOnly,allOrders)
+app.get('/:id',getSingleOrder)
+
 
 export default app;
