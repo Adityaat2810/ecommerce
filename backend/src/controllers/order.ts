5ad18b5239fa5b974ac903696ee2f30206569ede
Aditya Kumar Saini <adityasainics10@gmail.com>
2024-06-26 11:45:36 +0530

    created some order routes

--- backend/src/controllers/order.ts
+++ backend/src/controllers/order.ts
@@ -3,6 +3,7 @@ import { TryCatch } from "../middlewares/error.js";
 import { NewOrderRequestBody } from "../types/types.js";
 import { Order } from "../models/order.js";
 import { invalidateCache, reduceStock } from "../utils/features.js";
+import { myCache } from "../app.js";
 
 export const newOrder= TryCatch(
     async (req:Request<{},{},NewOrderRequestBody>,res,next)=>{
@@ -15,6 +16,13 @@ export const newOrder= TryCatch(
             total,
             shippingCharges,
             discount} = req.body
+
+        if(
+            !shippingInfo || !orderItems || !user || !subTotal || !tax || 
+            !total 
+        ){
+            return res.status(400).json({success:false,message:"Please fill all the fields"})
+        }
 
         await Order.create({
             shippingCharges,
@@ -35,6 +43,45 @@ export const newOrder= TryCatch(
             message:"Order placed successfully! "
         })
         
-        
     }
 )
+
+export const myOrders = TryCatch(
+    async (req,res ,next)=>{
+        const {id:user} = req.query;
+        let orders=[] ;
+        if(myCache.has(`my-orders-${user}`)) 
+            orders =JSON.parse(myCache.get(`my-orders-${user}`) as string) ;
+        else{
+            orders = await Order.find({user})
+            myCache.set(`my-orders-${user}`,JSON.stringify(orders))
+        }
+
+        return res.status(200).json({
+            success:true,
+            orders
+        })
+    }
+)
+
+
+export const allOrders = TryCatch(
+    async (req,res ,next)=>{
+        let orders=[] ;
+        if(myCache.has(`all-orders`)) 
+            orders =JSON.parse(myCache.get(`all-orders`) as string) ;
+        else{
+            orders = await Order.find()
+            myCache.set(`all-orders`,JSON.stringify(orders))
+        }
+
+        return res.status(200).json({
+            success:true,
+            orders
+        })
+    }
+)
+
+
+
+
