ffd86185c8f08cf24cfca813ed434e9b25c98e9b
Aditya Kumar Saini <adityasainics10@gmail.com>
2024-06-27 13:13:42 +0530

    created more payment routes

--- backend/src/controllers/payment.ts
+++ backend/src/controllers/payment.ts
@@ -20,3 +20,49 @@ export const newCoupon =TryCatch(
 
     }
 )
+
+export const applyDiscount =TryCatch(
+    async (req, res,next) => {
+        const {coupon}=req.query ;
+        const discount = await Coupon.findOne({couponCode:coupon})
+
+        if(!discount){
+            return next(new ErrorHandler("Invalid coupon code",400))
+        }
+
+        return res.status(201).json({
+            success:true,
+            discount:discount.amount
+        })
+
+    }
+)
+
+export const allCoupons =TryCatch(
+    async (req, res,next) => {
+        const coupons = await Coupon.find()
+
+        return res.status(201).json({
+            success:true,
+            coupons:coupons
+        })
+
+    }
+)
+
+export const deleteCoupon =TryCatch(
+    async (req, res,next) => {
+        const {id}=req.params ;
+        const coupon =await Coupon.findByIdAndDelete(id)
+
+        if(!coupon){
+            return next(new ErrorHandler("Invalid coupon id",400))
+        }
+        return res.status(201).json({
+            success:true,
+            message:"coupon dleted successfully"
+        })
+
+    }
+)
+
